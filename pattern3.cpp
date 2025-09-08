1813fd91b5274a941ee456304b5cde80aae21860
prince kumar <princek07194@gmail.com>
2025-09-08 22:45:19 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,63 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     //outer loop
+//     for(int i=1;i<=n;i++){
+//         //inner loop
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     //outer loop
+//     for(int i=n;i>=1;i--){
+//         //inner loop
+//         for(int j = 1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    //outer loop
+    for(int i=1;i<=n;i++){
+        //inner loop
+        //stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        //for spaces
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        //for stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    //lower half
+    for(int i=n;i>=1;i--){
+        //inner loop
+        //stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        //for spaces
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        //for stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
